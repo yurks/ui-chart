fc14f194d3317fd1694dba4fff817527b041adfe
Yurk Sha <yurkins@gmail.com>
2019-03-24 23:40:36 +0200

    update

--- assets/demo/helper.js
+++ assets/demo/helper.js
@@ -21,8 +21,26 @@
   var page = window.location.search.slice(1) || 'json';
   ajax('assets/demo/' + page + '.txt', function(data) {
     container.innerHTML = '';
-    uiChart(container, JSON.parse(data));
+    data = JSON.parse(data);
+
+    if (data && data.length) {
+      data.forEach(function(d, i) {
+        if (data.length > 1) {
+          var title = document.createElement('h2');
+          title.innerHTML = 'Dataset #' + (i+1);
+          container.appendChild(title);
+        }
+        uiChart(container, d);
+      })
+    } else if (data) {
+      uiChart(container, data);
+    }
+
   }, console.error);
+
+
+
+
 
 })(window);
 
