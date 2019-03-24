798da40e2852cc4a07c16be22623b0de655c5612
Yurk Sha <yurkins@gmail.com>
2019-03-24 17:49:55 +0200

    demo

--- assets/demo/helper.js
+++ assets/demo/helper.js
@@ -1,35 +1,28 @@
 (function(window) {
-    var noop = function() {};
+  var noop = function() {};
 
-    var ajax = function(url, success, error) {
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.onreadystatechange = function(){
-            if (request.readyState == 4) {
-                if (request.status >= 200 && request.status < 400) {
-                    (success||noop)(request.responseText);
-                } else {
-                    (error||noop)(request);
-                }
-            }
-        };
-        request.onerror = error||noop;
-        request.send();
+  var ajax = function(url, success, error) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onreadystatechange = function(){
+      if (request.readyState == 4) {
+        if (request.status >= 200 && request.status < 400) {
+          (success||noop)(request.responseText);
+        } else {
+          (error||noop)(request);
+        }
+      }
     };
+    request.onerror = error||noop;
+    request.send();
+  };
 
-    var container = document.getElementById('container');
-    var page = window.location.search.slice(1) || 'json';
-    if (page.indexOf('html') === 0) {
-        ajax('assets/demo/' + page + '.txt', function(data) {
-            container.innerHTML = data;
-            uiTree.init(container);
-        }, console.error);
-    } else {
-        ajax('assets/demo/' + page + '.txt', function(data) {
-            uiTree.draw(container, JSON.parse(data));
-        }, console.error);
-    }
-
+  var container = document.getElementById('container');
+  var page = window.location.search.slice(1) || 'json';
+  ajax('assets/demo/' + page + '.txt', function(data) {
+    container.innerHTML = '';
+    uiChart(container, JSON.parse(data));
+  }, console.error);
 
 })(window);
 
